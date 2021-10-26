95a29636ddd0c17546cdd9d127ae37d6af615aaf
Thomas Neis <thomas.neis@weareyou.com>
2021-10-26 11:48:32 +0200

    Update configs

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f https://github.com/ThomasNeis-WeAreYou/player.git main:gh-pages
+git push -f git@github.com:ThomasNeis-WeAreYou/player.git main:gh-pages
 
 cd -
